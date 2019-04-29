6f485742205280a419d388e1675ed9422e62dc1d
Kakeshi007 <kakeshi007@gmail.com>
2019-04-29 16:03:29 +0700

    update app name

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name' => 'บริษัทจัดเก็บขยะติดเชื้อ',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
