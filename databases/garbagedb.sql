74554193763554af549796c2f374c8e0b3ee6e5e
Wiriya Khetwit <noise@Wiriyas-MacBook-Pro.local>
2019-05-01 19:32:14 +0700

    add news customer db

--- databases/garbagedb.sql
+++ databases/garbagedb.sql
@@ -1,53 +1,99 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 01, 2019 at 02:28 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.28
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : garbagedb
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2019-04-29 01:56:36
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `garbagedb`
+--
 
--- ----------------------------
--- Table structure for `migration`
--- ----------------------------
-DROP TABLE IF EXISTS `migration`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CUSTOMER`
+--
+
+CREATE TABLE `CUSTOMER` (
+  `ID` int(11) NOT NULL,
+  `CUSTOMERNAME` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ADDRESS` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `OWNER` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `MOBILE` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `OFFICETEL` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `EMAIL` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `STATUS` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migration`
+--
+
 CREATE TABLE `migration` (
   `version` varchar(180) NOT NULL,
-  `apply_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`version`)
+  `apply_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of migration
--- ----------------------------
-INSERT INTO `migration` VALUES ('m000000_000000_base', '1556452023');
-INSERT INTO `migration` VALUES ('m130524_201442_init', '1556452026');
-INSERT INTO `migration` VALUES ('m140209_132017_init', '1556452556');
-INSERT INTO `migration` VALUES ('m140403_174025_create_account_table', '1556452556');
-INSERT INTO `migration` VALUES ('m140504_113157_update_tables', '1556452557');
-INSERT INTO `migration` VALUES ('m140504_130429_create_token_table', '1556452558');
-INSERT INTO `migration` VALUES ('m140830_171933_fix_ip_field', '1556452558');
-INSERT INTO `migration` VALUES ('m140830_172703_change_account_table_name', '1556452558');
-INSERT INTO `migration` VALUES ('m141222_110026_update_ip_field', '1556452558');
-INSERT INTO `migration` VALUES ('m141222_135246_alter_username_length', '1556452558');
-INSERT INTO `migration` VALUES ('m150614_103145_update_social_account_table', '1556452558');
-INSERT INTO `migration` VALUES ('m150623_212711_fix_username_notnull', '1556452558');
-INSERT INTO `migration` VALUES ('m151218_234654_add_timezone_to_profile', '1556452559');
-INSERT INTO `migration` VALUES ('m160929_103127_add_last_login_at_to_user_table', '1556452559');
-INSERT INTO `migration` VALUES ('m190124_110200_add_verification_token_column_to_user_table', '1556452026');
+--
+-- Dumping data for table `migration`
+--
 
--- ----------------------------
--- Table structure for `profile`
--- ----------------------------
-DROP TABLE IF EXISTS `profile`;
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1556452023),
+('m130524_201442_init', 1556452026),
+('m140209_132017_init', 1556452556),
+('m140403_174025_create_account_table', 1556452556),
+('m140504_113157_update_tables', 1556452557),
+('m140504_130429_create_token_table', 1556452558),
+('m140830_171933_fix_ip_field', 1556452558),
+('m140830_172703_change_account_table_name', 1556452558),
+('m141222_110026_update_ip_field', 1556452558),
+('m141222_135246_alter_username_length', 1556452558),
+('m150614_103145_update_social_account_table', 1556452558),
+('m150623_212711_fix_username_notnull', 1556452558),
+('m151218_234654_add_timezone_to_profile', 1556452559),
+('m160929_103127_add_last_login_at_to_user_table', 1556452559),
+('m190124_110200_add_verification_token_column_to_user_table', 1556452026);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `NEWS`
+--
+
+CREATE TABLE `NEWS` (
+  `ID` int(11) NOT NULL,
+  `NEWSTITLE` text COLLATE utf8_unicode_ci,
+  `NEWSCONTENT` text COLLATE utf8_unicode_ci,
+  `CREATEAT` datetime DEFAULT NULL,
+  `UPDATEAT` datetime DEFAULT NULL,
+  `CREATEBY` int(11) DEFAULT NULL,
+  `UPDATEBY` int(11) DEFAULT NULL,
+  `ISSHOW` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
 CREATE TABLE `profile` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -57,23 +103,25 @@ CREATE TABLE `profile` (
   `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `bio` text COLLATE utf8_unicode_ci,
-  `timezone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  `timezone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
--- Records of profile
--- ----------------------------
-INSERT INTO `profile` VALUES ('1', null, null, null, null, null, null, null, null);
-INSERT INTO `profile` VALUES ('2', null, null, null, null, null, null, null, null);
+--
+-- Dumping data for table `profile`
+--
 
--- ----------------------------
--- Table structure for `social_account`
--- ----------------------------
-DROP TABLE IF EXISTS `social_account`;
+INSERT INTO `profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`, `timezone`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_account`
+--
+
 CREATE TABLE `social_account` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `client_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -81,43 +129,38 @@ CREATE TABLE `social_account` (
   `code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` int(11) DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `account_unique` (`provider`,`client_id`),
-  UNIQUE KEY `account_unique_code` (`code`),
-  KEY `fk_user_account` (`user_id`),
-  CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
--- Records of social_account
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `token`
--- ----------------------------
-DROP TABLE IF EXISTS `token`;
+--
+-- Table structure for table `token`
+--
+
 CREATE TABLE `token` (
   `user_id` int(11) NOT NULL,
   `code` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` int(11) NOT NULL,
-  `type` smallint(6) NOT NULL,
-  UNIQUE KEY `token_unique` (`user_id`,`code`,`type`),
-  CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  `type` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
--- Records of token
--- ----------------------------
-INSERT INTO `token` VALUES ('1', 'jXqi21VJ9WqiiXpuRgM2bOYx4AbB_mS9', '1556453027', '0');
-INSERT INTO `token` VALUES ('2', 'EFeLmaebqMbMRjlI17YXi-MMz8jXG1UZ', '1556476857', '0');
+--
+-- Dumping data for table `token`
+--
 
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
+INSERT INTO `token` (`user_id`, `code`, `created_at`, `type`) VALUES
+(1, 'jXqi21VJ9WqiiXpuRgM2bOYx4AbB_mS9', 1556453027, 0),
+(2, 'EFeLmaebqMbMRjlI17YXi-MMz8jXG1UZ', 1556476857, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password_hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
@@ -129,14 +172,129 @@ CREATE TABLE `user` (
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   `flags` int(11) NOT NULL DEFAULT '0',
-  `last_login_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_unique_username` (`username`),
-  UNIQUE KEY `user_unique_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `last_login_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', 'kimniyom', 'kimniyomclub@hotmail.com', '$2y$12$wdb0dOW2hA5pZAlKhhUIxOaXv.7hVjTtde/Y8691zzhhSrFRLzeKi', 'ZZAuY9wCJptMlmuWCZKRg0DUcovrQKkl', null, null, null, '::1', '1556453027', '1556453027', '0', '1556476835');
-INSERT INTO `user` VALUES ('2', 'admin', 'admin@hotmail.com', '$2y$12$ZHSYDc.CNOI0qm8G2iBuDOirXM.x8TIXb00gf04KRcpLTkbn13fMK', 'tkgvzL8RuoCU7ARPO0xSCuXMbCnaxBvi', null, null, null, '::1', '1556476857', '1556476857', '0', null);
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `last_login_at`) VALUES
+(1, 'kimniyom', 'kimniyomclub@hotmail.com', '$2y$12$wdb0dOW2hA5pZAlKhhUIxOaXv.7hVjTtde/Y8691zzhhSrFRLzeKi', 'ZZAuY9wCJptMlmuWCZKRg0DUcovrQKkl', NULL, NULL, NULL, '::1', 1556453027, 1556453027, 0, 1556476835),
+(2, 'admin', 'admin@hotmail.com', '$2y$12$ZHSYDc.CNOI0qm8G2iBuDOirXM.x8TIXb00gf04KRcpLTkbn13fMK', 'tkgvzL8RuoCU7ARPO0xSCuXMbCnaxBvi', NULL, NULL, NULL, '::1', 1556476857, 1556476857, 0, NULL),
+(3, 'test1', 'test1@test.com', '$2y$13$Morc.SwxhMHrRhxrljw3pOqaHrf.mI/zLWa8.HpfeN6jkZ2gh1ZNK', 'N476SAwKzxUWppvj3f-gRBgy9K02xbiw', NULL, NULL, NULL, NULL, 1556382736, 1556382736, 0, 1556711645);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `CUSTOMER`
+--
+ALTER TABLE `CUSTOMER`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexes for table `NEWS`
+--
+ALTER TABLE `NEWS`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `CREATEBY` (`CREATEBY`),
+  ADD KEY `UPDATEBY` (`UPDATEBY`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `account_unique` (`provider`,`client_id`),
+  ADD UNIQUE KEY `account_unique_code` (`code`),
+  ADD KEY `fk_user_account` (`user_id`);
+
+--
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD UNIQUE KEY `token_unique` (`user_id`,`code`,`type`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_unique_username` (`username`),
+  ADD UNIQUE KEY `user_unique_email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `CUSTOMER`
+--
+ALTER TABLE `CUSTOMER`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `NEWS`
+--
+ALTER TABLE `NEWS`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `social_account`
+--
+ALTER TABLE `social_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `NEWS`
+--
+ALTER TABLE `NEWS`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`CREATEBY`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `news_ibfk_2` FOREIGN KEY (`UPDATEBY`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
