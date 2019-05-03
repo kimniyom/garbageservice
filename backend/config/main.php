616c82a398c03068337aed9afdde641d3e5e9dad
kimniyom <kimniyomclub@gmail.com>
2019-05-03 09:50:23 +0700

    update customer

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,75 +1,75 @@
 <?php
 
 $params = array_merge(
-        require __DIR__ . '/../../common/config/params.php', require __DIR__ . '/../../common/config/params-local.php', require __DIR__ . '/params.php', require __DIR__ . '/params-local.php'
+	require __DIR__ . '/../../common/config/params.php', require __DIR__ . '/../../common/config/params-local.php', require __DIR__ . '/params.php', require __DIR__ . '/params-local.php'
 );
 
 return [
-    'id' => 'app-backend',
-    'name' => 'Garbage service',
-    'basePath' => dirname(__DIR__),
-    'language' => 'th', // Set the language here
-    'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [
-        'user' => [
-            'class' => 'dektrium\user\Module',
-            'enableUnconfirmedLogin' => true,
-            'confirmWithin' => 21600,
-            'cost' => 12,
-            'admins' => ['admin']
-        ],
-        'customer' => [
-            'class' => 'app\modules\customer\Module',
-        ],
-    ],
-    'components' => [
-        'view' => [
-                 'theme' => [
-                     'pathMap' => [
-                         '@backend/views' => '@backend/themes/adminlte'
-                     ],
-                 ],
-        ],
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-        /*
-          'user' => [
-          'identityClass' => 'common\models\User',
-          'enableAutoLogin' => true,
-          'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-          ],
-         */
-        'user' => [
-            //'identityClass' => 'app\models\User',
-            'identityClass' => 'dektrium\user\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                    [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-    /*
-      'urlManager' => [
-      'enablePrettyUrl' => true,
-      'showScriptName' => false,
-      'rules' => [
-      ],
-      ],
-     */
-    ],
-    'params' => $params,
+	'id' => 'app-backend',
+	'name' => 'Garbage service',
+	'basePath' => dirname(__DIR__),
+	'language' => 'th', // Set the language here
+	'controllerNamespace' => 'backend\controllers',
+	'bootstrap' => ['log'],
+	'modules' => [
+		'user' => [
+			'class' => 'dektrium\user\Module',
+			'enableUnconfirmedLogin' => true,
+			'confirmWithin' => 21600,
+			'cost' => 12,
+			'admins' => ['admin'],
+		],
+		'customer' => [
+			'class' => 'app\modules\customer\Module',
+		],
+	],
+	'components' => [
+		'view' => [
+			'theme' => [
+				'pathMap' => [
+					'@backend/views' => '@backend/themes/adminlte',
+				],
+			],
+		],
+		'request' => [
+			'csrfParam' => '_csrf-backend',
+		],
+		/*
+			          'user' => [
+			          'identityClass' => 'common\models\User',
+			          'enableAutoLogin' => true,
+			          'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+			          ],
+		*/
+		'user' => [
+			//'identityClass' => 'app\models\User',
+			'identityClass' => 'dektrium\user\models\User',
+			'enableAutoLogin' => true,
+		],
+		'session' => [
+			// this is the name of the session cookie used for login on the backend
+			'name' => 'advanced-backend',
+		],
+		'log' => [
+			'traceLevel' => YII_DEBUG ? 3 : 0,
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		/*
+			      'urlManager' => [
+			      'enablePrettyUrl' => true,
+			      'showScriptName' => false,
+			      'rules' => [
+			      ],
+			      ],
+		*/
+	],
+	'params' => $params,
 ];
--- backend/modules/customer/controllers/CustomerController.php
+++ backend/modules/customer/controllers/CustomerController.php
@@ -2,181 +2,183 @@
 
 namespace app\modules\customer\controllers;
 
-use Yii;
 use app\modules\customer\models\Customer;
 use app\modules\customer\models\CustomerSearch;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
+use Yii;
 use yii\filters\VerbFilter;
 use yii\helpers\Json;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 /**
  * CustomerController implements the CRUD actions for Customer model.
  */
 class CustomerController extends Controller {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors() {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors() {
+		return [
+			'verbs' => [
+				'class' => VerbFilter::className(),
+				'actions' => [
+					'delete' => ['POST'],
+				],
+			],
+		];
+	}
 
-    /**
-     * Lists all Customer models.
-     * @return mixed
-     */
-    public function actionIndex() {
-        $searchModel = new CustomerSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+	/**
+	 * Lists all Customer models.
+	 * @return mixed
+	 */
+	public function actionIndex() {
+		$searchModel = new CustomerSearch();
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('index', [
-                    'searchModel' => $searchModel,
-                    'dataProvider' => $dataProvider,
-        ]);
-    }
+		return $this->render('index', [
+			'searchModel' => $searchModel,
+			'dataProvider' => $dataProvider,
+		]);
+	}
 
-    /**
-     * Displays a single Customer model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id) {
-        return $this->render('view', [
-                    'model' => $this->findModel($id),
-        ]);
-    }
+	/**
+	 * Displays a single Customer model.
+	 * @param integer $id
+	 * @return mixed
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	public function actionView($id) {
+		return $this->render('view', [
+			'model' => $this->findModel($id),
+		]);
+	}
 
-    /**
-     * Creates a new Customer model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate() {
-        $model = new Customer();
+	/**
+	 * Creates a new Customer model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 * @return mixed
+	 */
+	public function actionCreate() {
+		$model = new Customer();
 
-        if ($model->load(Yii::$app->request->post())) {
-            $model->CREATE_DATE = date("Y-m-d H:i:s");
-            $model->UPDATE_DATE = date("Y-m-d H:i:s");
-            $model->UPDATE_DATE = date("Y-m-d H:i:s");
-            $model->save();
-            return $this->redirect(['view', 'id' => $model->ID]);
-        }
+		if ($model->load(Yii::$app->request->post())) {
+			$model->CREATE_DATE = date("Y-m-d H:i:s");
+			$model->UPDATE_DATE = date("Y-m-d H:i:s");
+			$model->UPDATE_DATE = date("Y-m-d H:i:s");
+			$model->save();
+			return $this->redirect(['view', 'id' => $model->ID]);
+		}
 
-        return $this->render('create', [
-                    'model' => $model,
-        ]);
-    }
+		return $this->render('create', [
+			'model' => $model,
+		]);
+	}
 
-    /**
-     * Updates an existing Customer model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id) {
-        $model = $this->findModel($id);
+	/**
+	 * Updates an existing Customer model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id
+	 * @return mixed
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	public function actionUpdate($id) {
+		$model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ID]);
-        }
+		if ($model->load(Yii::$app->request->post()) && $model->save()) {
+			return $this->redirect(['view', 'id' => $model->ID]);
+		}
 
-        return $this->render('update', [
-                    'model' => $model,
-        ]);
-    }
+		return $this->render('update', [
+			'model' => $model,
+		]);
+	}
 
-    protected function MapData($datas, $fieldId, $fieldName) {
-        $obj = [];
-        foreach ($datas as $key => $value) {
-            array_push($obj, ['id' => $value->{$fieldId}, 'name' => $value->{$fieldName}]);
-        }
-        return $obj;
-    }
+	protected function MapData($datas, $fieldId, $fieldName) {
+		$obj = [];
+		foreach ($datas as $key => $value) {
+			array_push($obj, ['id' => $value->{$fieldId}, 'name' => $value->{$fieldName}]);
+		}
+		return $obj;
+	}
 
-    public function actionGetamphur() {
-        $out = [];
-        if (isset($_POST['depdrop_parents'])) {
-            $parents = $_POST['depdrop_parents'];
-            if ($parents != null) {
-                $province_id = $parents[0];
-                $datas = \app\models\Ampur::find()->where(['changwat_id' => $province_id])->all();
-                $out = $this->MapData($datas, 'ampur_id', 'ampur_name');
-                echo Json::encode(['output' => $out, 'selected' => '']);
-                return;
-            }
-        }
+	public function actionGetamphur() {
+		//\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+		$out = [];
+		if (isset($_POST['depdrop_parents'])) {
+			$parents = $_POST['depdrop_parents'];
+			if ($parents != null) {
+				$province_id = $parents[0];
+				$datas = \app\models\Ampur::find()->where(['changwat_id' => $province_id])->all();
+				$out = $this->MapData($datas, 'ampur_id', 'ampur_name');
+				return Json::encode(['output' => $out, 'selected' => '']);
+				//return ob_get_clean();
+			}
+		}
 
-        echo Json::encode(['output' => '', 'selected' => '']);
-    }
+		echo Json::encode(['output' => '', 'selected' => '']);
+	}
 
-    public function actionGettambon() {
-        $out = [];
-        if (isset($_POST['depdrop_parents'])) {
-            $parents = $_POST['depdrop_parents'];
-            if ($parents != null) {
-                $amphur_id = $parents[0];
-                $datas = \app\models\Tambon::find()->where(['ampur_id' => $amphur_id])->all();
-                $out = $this->MapData($datas, 'tambon_id', 'tambon_name');
-                echo Json::encode(['output' => $out, 'selected' => '']);
-                return;
-            }
-        }
+	public function actionGettambon() {
+		//\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+		$out = [];
+		if (isset($_POST['depdrop_parents'])) {
+			$parents = $_POST['depdrop_parents'];
+			if ($parents != null) {
+				$amphur_id = $parents[0];
+				$datas = \app\models\Tambon::find()->where(['ampur_id' => $amphur_id])->all();
+				$out = $this->MapData($datas, 'tambon_id', 'tambon_name');
+				return Json::encode(['output' => $out, 'selected' => '']);
+				//return;
+			}
+		}
 
-        echo Json::encode(['output' => '', 'selected' => '']);
-    }
+		echo Json::encode(['output' => '', 'selected' => '']);
+	}
 
-    public function actionGetzipcode() {
-        $out = [];
-        if (isset($_POST['depdrop_parents'])) {
-            $parents = $_POST['depdrop_parents'];
-            if ($parents != null) {
-                $district_id = $parents[0];
-                $datas = \app\models\Zipcodes::find()->where(['district_id' => $district_id])->all();
-                $out = $this->MapData($datas, 'zipcode', 'zipcode');
-                echo Json::encode(['output' => $out, 'selected' => '']);
-                return;
-            }
-        }
+	public function actionGetzipcode() {
+		$out = [];
+		if (isset($_POST['depdrop_parents'])) {
+			$parents = $_POST['depdrop_parents'];
+			if ($parents != null) {
+				$district_id = $parents[0];
+				$datas = \app\models\Zipcodes::find()->where(['district_id' => $district_id])->all();
+				$out = $this->MapData($datas, 'zipcode', 'zipcode');
+				echo Json::encode(['output' => $out, 'selected' => '']);
+				return;
+			}
+		}
 
-        echo Json::encode(['output' => '', 'selected' => '']);
-    }
+		echo Json::encode(['output' => '', 'selected' => '']);
+	}
 
-    /**
-     * Deletes an existing Customer model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id) {
-        $this->findModel($id)->delete();
+	/**
+	 * Deletes an existing Customer model.
+	 * If deletion is successful, the browser will be redirected to the 'index' page.
+	 * @param integer $id
+	 * @return mixed
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	public function actionDelete($id) {
+		$this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
-    }
+		return $this->redirect(['index']);
+	}
 
-    /**
-     * Finds the Customer model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Customer the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id) {
-        if (($model = Customer::findOne($id)) !== null) {
-            return $model;
-        }
+	/**
+	 * Finds the Customer model based on its primary key value.
+	 * If the model is not found, a 404 HTTP exception will be thrown.
+	 * @param integer $id
+	 * @return Customer the loaded model
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	protected function findModel($id) {
+		if (($model = Customer::findOne($id)) !== null) {
+			return $model;
+		}
 
-        throw new NotFoundHttpException('The requested page does not exist.');
-    }
+		throw new NotFoundHttpException('The requested page does not exist.');
+	}
 
 }
