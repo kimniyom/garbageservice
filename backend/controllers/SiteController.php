8f0f26b48313965a850f38ac1ae7784908940aac
Kakeshi007 <kakeshi007@gmail.com>
2019-06-14 16:37:08 +0700

    updte logout backend to frontend

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -95,7 +95,6 @@ class SiteController extends Controller
     public function actionLogout()
     {
         Yii::$app->user->logout();
-
-        return $this->goHome();
+        $this->redirect(Yii::$app->urlManagerFrontend->getBaseUrl());
     }
 }
